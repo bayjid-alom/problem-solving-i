e740dca01e6c7b89a416ebb08050c607e59ee25e
bayjidalom <bayjidalom1005@gmail.com>
2026-06-08 10:33:32 +0600

    miles to km - inch to feet

--- summary.js
+++ summary.js
@@ -13,3 +13,42 @@
  ❌ !=  ✅ !==
 
  */
+
+console.log("✅ _inch_to_feet | _miles_to_km")
+
+// 12 inch = 1 feet
+function inchToFeet(inch) {
+    const feet = inch / 12;
+    return feet;
+}
+
+function inchToFeet_2(inch) {
+    const feetFraction = inch / 12;
+    const feetNumber = parseInt(feetFraction)
+    const inchRemaining = inch % 12;
+    const result = feetNumber + " feet " + inchRemaining + " inch ";
+    return result;
+}
+
+const height = inchToFeet(66).toFixed(2);
+console.log(height)
+
+const height_2 = inchToFeet_2(66)
+console.log(height_2)
+
+
+// 1 kilometer = 0.621371 miles
+// 1 mile = 1.60934 kilometer
+function mileToKm(mile) {
+    const km = 1.60934 * mile;
+    const result = km.toFixed(3) + " Kilometers ";
+    return result;
+    
+}
+
+const distance = mileToKm(10);
+console.log(distance)
+
+
+
+// console.log("✅ _inch_to_feet | _miles_to_km")
